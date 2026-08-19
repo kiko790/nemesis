3cb6ee0e221fa25d42139deebe226269636a028c
ykknzo-hub <kenzielimonn@gmail.com>
2026-08-19 01:44:42 +0100

    Update NEMESIS NT.lua

--- NEMESIS NT.lua
+++ NEMESIS NT.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ykknzo-hub/notid/refs/heads/main/notfications.lua"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/hSjwE0EW"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/2M75zrTQ"))()
 
 local HttpService = game:GetService("HttpService")
 local plrs = game:GetService("Players")
